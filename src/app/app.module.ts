060cae1e1ec3d15baadb37ba8a6cea0dfc78ee63
Tincho <cavalieri.martin@gmail.com>
2021-07-25 13:45:04 -0300

    uyk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,14 +23,16 @@ import { LoadingComponent } from './common/loading/loading.component';
     AddTransactionComponent,
     ViewTransactionsComponent,
     UpdateTransactionComponent,
-    LoadingComponent
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/transactions/add-transaction/add-transaction.component.ts
+++ src/app/transactions/add-transaction/add-transaction.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Transaction } from 'src/app/models/transaction';
+import { Wallet } from 'src/app/models/wallet';
+import { TransactionService } from 'src/app/services/transaction.service';
+import { WalletService } from 'src/app/services/wallet.service';
 
 @Component({
   selector: 'app-add-transaction',
   templateUrl: './add-transaction.component.html',
-  styleUrls: ['./add-transaction.component.scss']
+  styleUrls: ['./add-transaction.component.scss'],
 })
 export class AddTransactionComponent implements OnInit {
+  newTransactionForm: FormGroup | undefined;
+  transaction = {} as Transaction;
+  wallets: Wallet[] | undefined;
 
-  constructor() { }
+  constructor(
+    private transactionService: TransactionService,
+    private walletService: WalletService,
+    public formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAllWallets();
+
+    this.newTransactionForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      walletId: ['', Validators.required],
+      description: [''],
+      date: [Date()],
+      time: [Date()],
+      amount: ['', Validators.required],
+    });
   }
 
+  getAllWallets() {
+    this.walletService.getWallets().subscribe((wallets) => {
+      this.wallets = wallets;
+    });
+  }
+
+  addTransaction() {
+    if (this.newTransactionForm?.valid) {
+      console.log(this.newTransactionForm.value);
+      this.transactionService.createTransaction(this.newTransactionForm.value);
+      this.transaction = {} as Transaction;
+    }
+  }
 }
